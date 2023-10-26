c84f233cdc57993ddfd42eaa1d994e25abd09f2f
GABRIEL BARRETO <gabrielbarreto_user@hotmail.com>
2023-10-25 21:48:04 -0300

    iniciar a tela de monitoramento das salas

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -8,7 +8,17 @@ import { RoomsService } from '../services/rooms.service';
 })
 export class PainelComponent implements OnInit {
 
-  salas: any;
+  salas = [
+  {numSala: '301B', emergencia: true},
+  {numSala: '302B', emergencia: false},
+  {numSala: '303B', emergencia: true},
+  {numSala: '304B', emergencia: false},
+  {numSala: '305B', emergencia: false},
+  {numSala: '306B', emergencia: true},
+  {numSala: '307B', emergencia: false},
+  {numSala: '308B', emergencia: true}
+];
+
 
 constructor(private room_service: RoomsService){
 
@@ -22,7 +32,7 @@ ngOnInit(){
 obterMapaSalas(){
 
   this.room_service.getSalas().subscribe(data => {
-    this.salas = data;
+    // this.salas = data;
   })
 
 }
